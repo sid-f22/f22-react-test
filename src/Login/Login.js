e9bf012ec0d33f695a1a941398d0f0980395aa0b
jenipha <jenipharachel@gmail.com>
2019-09-21 02:40:54 +0530

    pages created

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -3,7 +3,22 @@ import "./Login.css";
 
 class Login extends Component {
   render() {
-    return <div className="Login"></div>;
+    return (
+      <div className="Login">
+        <h1>
+          Log in
+          <br /> to get
+          <br /> Offers
+        </h1>
+        <form className="login_form">
+          <input type="email" id="email" placeholder="Email Address" />
+          <input type="password" placeholder="Password" />
+          <button type="submit" className="sign">
+            Sign Up Now
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
--- src/SignUp/SignUp.js
+++ src/SignUp/SignUp.js
@@ -3,7 +3,19 @@ import "./SignUp.css";
 
 class SignUp extends Component {
   render() {
-    return <div className="Sign_Up"></div>;
+    return (
+      <div className="Sign_Up">
+        <form className="sign_form">
+          <input type="text" id="name" placeholder="Name" />
+          <input type="email" placeholder="Email Address" />
+          <input type="number" placeholder="Phone Number" />
+          <input type="password" placeholder="Password" />
+          <button type="submit" className="sign">
+            Sign Up Now
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
